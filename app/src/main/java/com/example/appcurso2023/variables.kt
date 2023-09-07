7a9587c9b26b5883e107f4a550f17927be3cd5bc
DeyvisDev <97794872+DeyvisDev@users.noreply.github.com>
2023-09-07 17:57:30 -0400

    gggg

--- app/src/main/java/com/example/appcurso2023/variables.kt
+++ app/src/main/java/com/example/appcurso2023/variables.kt
@@ -1,9 +1,43 @@
 package com.example.appcurso2023
+//VARIABLES
 
 fun main(){
-    print("hola")
-    print("hola")
-    print("hola")
-    print("hola")
-    print("hola")
+
+    //edad(25)
+    var nombre:String = "Deyvis"
+    mostrarminombre(nombre)
+
+}
+fun mostrarminombre(nombre:String){
+    nombre
+    println("My name is $nombre")
+}
+fun edad(int: Int){
+    print(int)
+}
+fun variables(){
+    val name = "DeyvisDev"
+    //variables numericas
+    //int
+    val number:Int = 3424
+    //long
+    val age: Long = 2523323232131
+    //float
+    val floatEjemploDeUnFlotante:Float = 30.5f
+    //double
+    val double:Double = 3434.4342
+
+    //variables alfanumericas
+    //char
+    val charExample1:Char = 'e'
+    val charExample2:Char = '1'
+    val charExample3:Char = '#'
+    //String
+    val stringExample:String = "saseeee "
+    //Variables boleanas
+    //boolean
+    val boleanExamples:Boolean = true
+
+    var a = 20
+    var b = 10
 }
