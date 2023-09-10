69db2532a234275271dafac15fe895304370c350
DeyvisDev <97794872+DeyvisDev@users.noreply.github.com>
2023-09-10 00:04:23 -0400

    listfinal

--- app/src/main/java/com/example/appcurso2023/List.kt
+++ app/src/main/java/com/example/appcurso2023/List.kt
@@ -1,7 +1,9 @@
 package com.example.appcurso2023
 
+import kotlinx.coroutines.processNextEventInCurrentThread
+
 fun main(){
-    inmutableList()
+    mutableList()
 }
 fun inmutableList(){
     val readOnly:List<String> = listOf("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
@@ -11,8 +13,48 @@ fun inmutableList(){
     //println(readOnly.last())
     //println(readOnly.first())
 
+
+    //filtar
     val example = readOnly.filter { it.contains("i") }
     //println(example)
+    //iterar
     readOnly.forEach { weekDay ->println(weekDay)}
+}
+fun mutableList(){
+    var weekDays:MutableList<String> = mutableListOf("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+    println(weekDays)
 
+    //añadimos al final
+    weekDays.add("diaExtra")
+    println(weekDays)
+
+    //añadir segun el indice
+    weekDays.add(0,"deyvisdev")
+    println(weekDays)
+
+    //ver si esta vacia la lista
+    if (weekDays.isEmpty()){
+        //no se hace nada
+    }else  {
+        weekDays.forEach{ println(it)}
+    }
+    //otra forma
+
+    if (weekDays.isNotEmpty()){
+        weekDays.forEach { println(it) }
+    }
+
+    //mostrar ultomo
+    weekDays.last()
+
+    //fors
+    //for normal
+    for (item in weekDays){
+        println(item)
+    }
+    weekDays.add("dianavidad")
+    //for facil
+    weekDays.forEach{
+        println(it)
+    }
 }
