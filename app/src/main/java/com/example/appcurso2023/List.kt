829e7f1cec4a47d4c8d7f788a3ec1d8b781356e3
DeyvisDev <97794872+DeyvisDev@users.noreply.github.com>
2023-09-09 23:55:11 -0400

    list2

--- app/src/main/java/com/example/appcurso2023/List.kt
+++ app/src/main/java/com/example/appcurso2023/List.kt
@@ -5,9 +5,14 @@ fun main(){
 }
 fun inmutableList(){
     val readOnly:List<String> = listOf("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
-    println(readOnly.size)
-    println(readOnly)
-    println(readOnly[0])
-    println(readOnly.last())
-    println(readOnly.first())
+    //println(readOnly.size)
+    //println(readOnly)
+    //println(readOnly[0])
+    //println(readOnly.last())
+    //println(readOnly.first())
+
+    val example = readOnly.filter { it.contains("i") }
+    //println(example)
+    readOnly.forEach { weekDay ->println(weekDay)}
+
 }
