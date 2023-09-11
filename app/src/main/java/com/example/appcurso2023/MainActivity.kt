aeef4799c4f09cb9f6ff8108134074fb920972e0
DeyvisDev <97794872+DeyvisDev@users.noreply.github.com>
2023-09-11 19:34:09 -0400

    edittext button

--- app/src/main/java/com/example/appcurso2023/MainActivity.kt
+++ app/src/main/java/com/example/appcurso2023/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.appcurso2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStar = findViewById<AppCompatButton>(R.id.btnP)
+        val edittextname = findViewById<AppCompatEditText>(R.id.etname)
+        var name:String = ""
+
+
+        btnStar.setOnClickListener{
+            name = edittextname.text.toString()
+            Log.i("msg","Pulsaste el Boton $name")
+
+        }
 
 
     }
